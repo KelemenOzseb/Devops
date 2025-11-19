cc3d0591d499bcbdc5998841b269072e93b5f4c6
Kelemen Özséb <kelemenozzseb@gmail.com>
2025-11-19 21:51:59 +0100

    ready for devops

---
+++ GyakorloFeladat/GyakorloFeladat/Controllers/SongController.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace GyakorloFeladat.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SongController : ControllerBase
+    {
+        private readonly IRepository<Song> _repository;
+
+        public SongController(IRepository<Song> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<List<Song>> GetAll()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddSong(string artist, string title, int length, int likes)
+        {
+            await _repository.Create(new Song(artist, title, length, likes));
+            return Ok();
+        }
+    }
+}
